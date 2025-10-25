30f6d070a20763e7a611bda325bd01d992a5e250
RAULA-CN31E83\raulA <rauladan99@gmail.com>
2025-10-25 19:47:50 +0200

    Creación del core con las entidades encapsuladas para evitar la modificación de item

---
+++ GildedRose/Core/ItemEncapsuladoAgedBrie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata.Core
+{
+    public class ItemEncapsuladoAgedBrie : ItemEncapsuladoBase
+    {
+        public ItemEncapsuladoAgedBrie(Item item) : base(item)
+        {
+        }
+
+        public override void UpdateQuality()
+        {
+            // 1. Aumentar la calidad en 1 punto
+            IncreaseQuality();
+
+            // 2. Reducir el SellIn en 1 (pasa un día)
+            DecreaseQuality();
+
+            //3. Si el ítem ha expirado , (SellIn < 0 ) aumentar la calidad otra vez
+
+            if (IsExpired)
+            {
+                IncreaseQuality();
+            }
+        }
+    }
+}
---
+++ GildedRose/Core/ItemEncapsuladoNormal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata.Core
+{
+    /// <summary>
+    /// Representa un ítem normal dentro del sistema.
+    /// Los ítems normales degradan su calidad en 1 por día,
+    /// y el doble de rápido una vez pasada la fecha de venta (SellIn < 0).
+    /// </summary>
+    public class NormalItemEncapsulado : ItemEncapsuladoBase
+    {
+        public NormalItemEncapsulado(Item item) : base(item)
+        {
+        }
+
+        public override void UpdateQuality()
+        {
+            // 1. Disminuir la calidad en 1 unidad (si aún tiene calidad > 0)
+            DecreaseQuality();
+
+            // 2. Reducir el SellIn en 1 (pasa un día)
+            DecreaseSellIn();
+
+            // 3. Si el ítem ya ha expirado, vuelve a disminuir la calidad
+            if (IsExpired)
+            {
+                DecreaseQuality();
+            }
+        }
+    }
+}
